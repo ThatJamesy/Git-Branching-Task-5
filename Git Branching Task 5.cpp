9a1ef0871700d4947c2dcc6f0482c1596460e17c
ThatJamesy <james.neal733@yahoo.com.au>
2025-04-09 10:30:11 +0930

    First Commit In Feat onboarding
    first one yes

--- Git Branching Task 5.cpp
+++ Git Branching Task 5.cpp
@@ -5,6 +5,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "AHHHHHHHHHHHHHH\n";
+    std::cout << "Welcome to the Australian Science program-aided enrichment center." << std::endl;
+    std::cout << "We hope your time in the classroom has been a pleasant one." << std::endl;
+    std::cout << "For your own safety and the safety of others, please ______." << std::endl;
 }
 
